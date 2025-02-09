7119af58f8fe98185816152b4d5b28c307c744cd
Kunal Farmah <kunalfarmah98@gmail.com>
2025-02-09 17:15:37 +0530

    disabled dark theme

--- app/src/main/java/com/apps/kunalfarmah/kpass/ui/theme/Theme.kt
+++ app/src/main/java/com/apps/kunalfarmah/kpass/ui/theme/Theme.kt
@@ -1,10 +1,7 @@
 package com.apps.kunalfarmah.kpass.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -12,11 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-)
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
@@ -33,7 +25,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun KPassTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -43,8 +35,6 @@ fun KPassTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
