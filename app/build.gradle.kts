97b07f7297653ce260eab7fe52afece9b8eae94e
Kunal Farmah <kunalfarmah98@gmail.com>
2025-02-12 21:16:38 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.apps.kunalfarmah.kpass"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String","KEY_MASTER", keyMaster)
     }
